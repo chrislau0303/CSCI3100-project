c4a7eedc5e86101ed1dbc3091dba910f82f86d85
chrislau0303 <102238630+chrislau0303@users.noreply.github.com>
2023-04-26 00:54:02 +0800

    Update profile.js

--- public/profile.js
+++ public/profile.js
@@ -1,9 +1,9 @@
 // const popupcreate = document.querySelector('.popupcreatepost');
-const popupedit = document.querySelector('.popupeditpost');
-// const postcreatebtn = document.querySelector('.createpostbtn');
+// const popupedit = document.querySelector('.popupeditpost');
+// const postcreatebtn = document.querySelector('#opencreatepage');
 const editbtn = document.querySelectorAll('.popupeditbtn');
 // const closeModalButton = document.querySelector('#closecreatepage');
-const closeeditbtn = document.querySelector('#closeedit');
+const closeeditbtn = document.querySelectorAll('.close-button');
 const submitbtn = document.querySelector('#submit');
 const inputElm = document.querySelector('.textarea');
 const chatarea = document.querySelector('.comment-widgets');
@@ -29,7 +29,7 @@ submitbtn.addEventListener('click', () => {
     console.log(userInput);
     let temp = `<div class="d-flex flex-row comment-row m-t-0">
     <div class="p-2"></div><div class="comment-text w-100">
-    <h6 class="font-medium">Dennis Law</h6> <span class="m-b-15 d-block">${userInput}</span>
+    <h6 class="font-medium">Chris Wong</h6> <span class="m-b-15 d-block">${userInput}</span>
     <div class="comment-footer"> </div>
     </div>
 </div>`;
@@ -44,31 +44,36 @@ submitbtn.addEventListener('click', () => {
 // closeModalButton.addEventListener('click', () => {
 //   popupcreate.style.display = 'none';
 // });
-
-// Add an event listener to each "Edit" button
-const postSize = document.getElementById('postSize').value;
-for (let i = 0; i < postSize; i++) {
-    editbtn[i].addEventListener('click', () => {
-        const postId = editbtn[i].dataset.postId;
-        const form = document.querySelector(`#edit-form-${postId}`);
-        form.querySelector('#content').value = editbtn[i].dataset.postDes;
-        form.querySelector('#hide_post').checked = editbtn[i].dataset.hidePost === 'true';
-        form.querySelector('#postID').value = postId;
-        console.log(editbtn[i].dataset.postDes)
-        popupedit.classList.toggle('show');
-        popupedit.style.display = 'block';
-    });
-}
-
-closeeditbtn.addEventListener('click', () => {
-  popupedit.style.display = 'none';
+editbtn.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the default click behavior
+    const postId = btn.dataset.postId;
+    const form = document.querySelector(`#edit-form-${postId}`);
+    const popupedit = form.closest('.popupeditpost'); // Get the parent popup element
+      // Set the value of the form fields using the post details
+      form.querySelector('#content').value = btn.dataset.postDes;
+      form.querySelector('#hide_post').checked = btn.dataset.hidePost === 'true';
+      form.querySelector('#postID').value = postId;
+      
+      popupedit.classList.toggle('show');
+      popupedit.style.display = 'block';
+  });
 });
 
-window.addEventListener('click', function (event) {
-  if (event.target === popupcreate) {
-    popupcreatepost.style.display = 'none';
-  }
-  if (event.target === popupedit) {
-    popupeditpost.style.display = 'none';
-  }
+closeeditbtn.forEach(button => {
+  button.addEventListener('click', () => {
+    const popupedit = button.closest('.popupeditpost');
+    popupedit.style.display = 'none';
+  });
 });
+
+// window.addEventListener('click', function (event) {
+//   if (event.target === popupcreate) {
+//     popupcreatepost.style.display = 'none';
+//   }
+// });
+// window.addEventListener('click', function (event) {
+//   if (event.target === popupedit) {
+//     popupeditpost.style.display = 'none';
+//   }
+// });
