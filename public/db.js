1079058e781255731aac3f6943cc5e3ee10d8d37
chrislau0303 <102238630+chrislau0303@users.noreply.github.com>
2024-08-15 11:34:16 +0800

    Update db.js

--- public/db.js
+++ public/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Abcd0303',
+    password: 'yourdatabasepassword',
     database: 'rettiwt'
 })
 
